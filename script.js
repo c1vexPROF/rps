98265dea99bb3ca1d3277a50b8f60836e3a22647
ersan <90734204+c1vexPROF@users.noreply.github.com>
2022-05-13 17:00:41 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let choice_player2;
 
 let player1div=document.querySelector(".choice-player-1")
 let player2div=document.querySelector(".choice-player-2")
+let pos1=document.querySelector(".pos1")
+let pos2=document.querySelector(".pos2")
+let icon=document.querySelector(".icon")
+let txt=document.querySelectorAll(".txt")
 
 document.addEventListener("keydown", function(arg){
 	if(player1==true && player2==false){
@@ -12,22 +16,25 @@ document.addEventListener("keydown", function(arg){
             console.log("Выбран камень у игрока 1")
             player1=false;
             player2=true;
-            document.querySelector(".info").innerHTML="Игрок 2 ходит!"
+            document.querySelector(".info").innerHTML="Синий игрок ходит!"
             choice_player1="Камень";
+            player1div.style.backgroundImage="url("+"loading.gif"+")";
         }
         else if(arg.code=="KeyS"){
             console.log("Выбраны ножницы у игрока 1")
             player1=false;
             player2=true;
-            document.querySelector(".info").innerHTML="Игрок 2 ходит!"
+            document.querySelector(".info").innerHTML="Синий игрок ходит!"
             choice_player1="Ножницы";
+            player1div.style.backgroundImage="url("+"loading.gif"+")";
         }
         else if(arg.code=="KeyD"){
         	console.log("Выбрана бумага у игрока 1")
         	player1=false;
             player2=true;
-            document.querySelector(".info").innerHTML="Игрок 2 ходит!"
+            document.querySelector(".info").innerHTML="Синий игрок ходит!"
             choice_player1="Бумага";
+            player1div.style.backgroundImage="url("+"loading.gif"+")";
         }
 
 	}
@@ -36,22 +43,25 @@ document.addEventListener("keydown", function(arg){
             console.log("Выбран камень у игрока 2")
             player2=false;
             choice_player2="Камень";
-            setTimeout(showItems,500)
-            setTimeout(finish,1500)
+            player2div.style.backgroundImage="url("+"loading.gif"+")";
+            setTimeout(showItems,1500)
+            setTimeout(finish,2500)
         }
         else if(arg.code=="KeyK"){
             console.log("Выбраны ножницы у игрока 2")
             player2=false;
             choice_player2="Ножницы";
-            setTimeout(showItems,500)
-            setTimeout(finish,1500)
+            player2div.style.backgroundImage="url("+"loading.gif"+")";
+            setTimeout(showItems,1500)
+            setTimeout(finish,2500)
         }
         else if(arg.code=="KeyL"){
         	console.log("Выбрана бумага у игрока 2")
         	player2=false;
         	choice_player2="Бумага";
-        	setTimeout(showItems,500)
-        	setTimeout(finish,1500)
+        	player2div.style.backgroundImage="url("+"loading.gif"+")";
+        	setTimeout(showItems,1500)
+        	setTimeout(finish,2500)
         }
 	}
 })
@@ -79,19 +89,55 @@ function showItems(){
 }
 function finish() {
 	if(choice_player1=="Камень" && choice_player2=="Ножницы"){
-		alert("Игрок 1 победил!")
+		pos1.style.backgroundImage="url("+"win.gif"+")";
+		alert("Победа красных!");
+		txt[0].style.display="none";
+		txt[1].style.display="none";
+		txt[2].style.display="none";
+		txt[3].style.display="none";
+		txt[4].style.display="none";
+		txt[5].style.display="none";
 	}
 	else if(choice_player1=="Ножницы" && choice_player2=="Бумага"){
-		alert("Игрок 1 победил!")
+		pos1.style.backgroundImage="url("+"win.gif"+")";
+		alert("Победа красных!");
+		txt[0].style.display="none";
+		txt[1].style.display="none";
+		txt[2].style.display="none";
+		txt[3].style.display="none";
+		txt[4].style.display="none";
+		txt[5].style.display="none";
 	}
 	else if(choice_player1=="Бумага" && choice_player2=="Камень"){
-		alert("Игрок 1 победил!")
+		pos1.style.backgroundImage="url("+"win.gif"+")";
+		alert("Победа красных!");
+		txt[0].style.display="none";
+		txt[1].style.display="none";
+		txt[2].style.display="none";
+		txt[3].style.display="none";
+		txt[4].style.display="none";
+		txt[5].style.display="none";
 	}
 	else if(choice_player1==choice_player2){
-		alert("Ничья")
+		pos1.style.backgroundImage="url("+"win.gif"+")";
+		pos2.style.backgroundImage="url("+"win.gif"+")";
+		alert("Ничья");
+		txt[0].style.display="none";
+		txt[1].style.display="none";
+		txt[2].style.display="none";
+		txt[3].style.display="none";
+		txt[4].style.display="none";
+		txt[5].style.display="none";
 	}
 	else{
-		alert("Игрок 2 победил!")
+		alert("Победа синих!")
+		pos2.style.backgroundImage="url("+"win.gif"+")";
+		txt[0].style.display="none";
+		txt[1].style.display="none";
+		txt[2].style.display="none";
+		txt[3].style.display="none";
+		txt[4].style.display="none";
+		txt[5].style.display="none";
 	}
 
 }
